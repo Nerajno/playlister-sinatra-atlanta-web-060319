cec6b9b8b3f439a62444fd81dac7f6fbc4490346
MaryJay787 <josephmaryam.mj@gmail.com>
2019-06-27 18:03:37 -0400

    Still Working

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -3,6 +3,7 @@ set :views, 'app/views/songs'
 
   get '/new' do
     @genres = Genre.all
+    @song = Song.new
     erb :new
   end
 
@@ -21,6 +22,14 @@ set :views, 'app/views/songs'
   #   end
   # redirect "/songs/#{@song.id}"
   # end
+
+  post '/songs' do
+    @artist = Artist.create(name: params[:song][:artist][0][:name]),
+    @song = Song.create(params[:song])
+    binding.pry
+    this 
+    erb :new
+  end
 
   get '/songs/:id' do
     @song = Song.find(params[:id])
